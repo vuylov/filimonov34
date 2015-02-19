b4243744f173b5603110d92fed4dab13885d42f6
vuylov <vuylov@gmail.com>
2015-02-19 14:54:30 +0300

    Change delay slider

--- web/js/init.js
+++ web/js/init.js
@@ -4,6 +4,7 @@ $('.bxslider').bxSlider({
     video: true,
     auto: true,
     speed: 2500,
+    pause: 6000,
     nextText: '<span class="glyphicon glyphicon-chevron-right bg-arrow next-arrow"></span>',
     prevText: '<span class="glyphicon glyphicon-chevron-left bg-arrow prev-arrow"></span>'
 });
