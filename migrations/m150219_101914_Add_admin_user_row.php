64bc4c08901d9bf3133c305ae7fe3cffd7b0182f
vuylov <vuylov@gmail.com>
2015-02-19 14:48:13 +0300

    Add migration for add admin user

---
+++ migrations/m150219_101914_Add_admin_user_row.php
@@ -0,0 +1,20 @@
+<?php
+use yii\db\Migration;
+
+class m150219_101914_Add_admin_user_row extends Migration
+{
+    public function up()
+    {
+        $this->insert('user', [
+            'name'      => 'Вуйлов Дмитрий Андреевич',
+            'email'     => 'vuylov@gmail.com',
+            'password'  => Yii::$app->security->generatePasswordHash('power2008', 20),
+            'role'      => 1,
+        ]);
+    }
+
+    public function down()
+    {
+        $this->delete('user', 'email = :email', [':email' => 'vuylov@gmail.com']);
+    }
+}
