d31cddac2416613716474d5ed54352dfbd9e344c
vuylov <vuylov@gmail.com>
2015-02-27 14:33:37 +0300

    Merge commit

--- views/news/view.php
+++ views/news/view.php
@@ -5,11 +5,9 @@ use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\News */
-
 $this->title = $model->title;
 ?>
 <div class="news-view">
-
     <h3 class="section-header"><?= Html::encode($this->title) ?></h3>
     <?php if(!Yii::$app->user->isGuest):?>
         <p>
@@ -24,30 +22,10 @@ $this->title = $model->title;
         </p>
     <?php endif;?>
     <div  class="news-view">
-        <?php// if($model->thumb):?>
-            <?//=Html::img(Yii::$app->homeUrl.'upload/news/'.$model->thumb, ['class' => 'thumbnail', 'style' => 'float:left; margin-right: 10px']);?>
-        <?php// endif;?>
         <?=$model->description;?>
         <div class="clearfix"></div>
         <div class="pull-right">
             <?=Html::a('Вернуться назад', ['site/index'], ['class' => 'btn btn-default']);?>
         </div>
     </div>
-    <?/*= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            //'id',
-            'user.name',
-            'newstype.name',
-            'active',
-            'title',
-            'description:html',
-            'thumb',
-            'visitors',
-            'create_at',
-            'keywords:ntext',
-            'description_seo:ntext',
-        ],
-    ]) */?>
-
 </div>
