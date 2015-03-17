7ec9cadbf33c8290484ea4f96ef12b24559b0f59
vuylov <vuylov@gmail.com>
2015-03-17 13:51:26 +0300

    Fix img path

--- views/site/partners.php
+++ views/site/partners.php
@@ -4,6 +4,6 @@ use yii\helpers\Html;
 <h3 class="section-header">Партнеры</h3>
 <div>
     <a href="http://mspit.ru" target="_blank">
-        <?= Html::img('@webroot/images/static/mspit-logo.png');?>
+        <?= Html::img('@web/images/static/mspit-logo.png');?>
     </a>
 </div>
